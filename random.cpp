ad2fd74e3d7c715d91957af847c0f2c8bb296c66
Amol Dhamale <amoldhamale1105@gmail.com>
2022-04-12 14:48:45 +0530

    removed redundant comments

--- random.cpp
+++ random.cpp
@@ -4,42 +4,42 @@
  
 int main() 
 {
-    std::srand(1); // use current time as seed for random generator
+    std::srand(1);   
     int random_variable = std::rand();
     std::cout << "Random value on [0 " << RAND_MAX << "]: " 
               << random_variable << '\n';
  
-    // roll 6-sided dice 20 times
+     
     for (int n=0; n != 5; ++n) {
         std::cout<<std::rand()<<std::endl;
     }
 
-    std::srand(2); // use current time as seed for random generator
+    std::srand(2);   
     random_variable = std::rand();
     std::cout << "Random value on [0 " << RAND_MAX << "]: " 
               << random_variable << '\n';
  
-    // roll 6-sided dice 20 times
+     
     for (int n=0; n != 5; ++n) {
         std::cout<<std::rand()<<std::endl;
     }
 
-    std::srand(1); // use current time as seed for random generator
+    std::srand(1);   
     random_variable = std::rand();
     std::cout << "Random value on [0 " << RAND_MAX << "]: " 
               << random_variable << '\n';
  
-    // roll 6-sided dice 20 times
+     
     for (int n=0; n != 5; ++n) {
         std::cout<<std::rand()<<std::endl;
     }
 
-    std::srand(2); // use current time as seed for random generator
+    std::srand(2);   
     random_variable = std::rand();
     std::cout << "Random value on [0 " << RAND_MAX << "]: " 
               << random_variable << '\n';
  
-    // roll 6-sided dice 20 times
+     
     for (int n=0; n != 5; ++n) {
         std::cout<<std::rand()<<std::endl;
     }
