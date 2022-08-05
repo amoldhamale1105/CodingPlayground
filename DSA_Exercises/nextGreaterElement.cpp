b490af34b0b26bc9b323b1c4d0b852750b03b2b2
amoldhamale1105 <amoldhamale1105@gmail.com>
2022-08-05 15:08:09 +0200

    add stack DS exercise solution

---
+++ DSA_Exercises/nextGreaterElement.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <algorithm>
+
+std::ostream& operator<<(std::ostream& stream, const std::vector<int>& vec)
+{
+    stream <<"[";
+    for(const int& num : vec)
+    {
+        stream << num << " ";
+    }
+    stream<<'\b';
+    stream<<"]";
+    
+    return stream;
+}
+
+std::vector<int> nextGreaterElement(const std::vector<int>& v)
+{
+    int n = v.size();
+    std::stack<int> stack;
+    std::vector<int> nextGreaterContainer;
+
+    for(auto i = n-1; i >= 0; --i)
+    {
+        while (!stack.empty() && stack.top() <= v[i])
+        {
+            stack.pop();
+        }
+        if (stack.empty())
+            nextGreaterContainer.emplace_back(-1);
+        else
+            nextGreaterContainer.emplace_back(stack.top());
+
+        stack.push(v[i]);
+    }
+
+    std::reverse(nextGreaterContainer.begin(), nextGreaterContainer.end());
+
+    return nextGreaterContainer;
+}
+
+int main()
+{
+    std::vector<int> vec = {1,6,2,7,25};
+    std::cout<<"Next greater element set: "<<nextGreaterElement(vec)<<std::endl;
+    return 0;
+}
