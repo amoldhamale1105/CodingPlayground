58d557a4bf2aaf8aaf4fbcc6ca6524e81a0ff629
Amol Dhamale <amoldhamale1105@gmail.com>
2022-09-18 15:47:29 +0530

    eliminate external input for preorder tree map by setting up build method with vector

--- DSA_Exercises/BinaryTree/kthLevel.cpp
+++ DSA_Exercises/BinaryTree/kthLevel.cpp
@@ -9,22 +9,32 @@ struct Node
     Node* left, *right;
 };
 
-Node* buildTree()
+Node* buildTreePreorder(std::vector<int>::const_iterator& itr, const std::vector<int>::const_iterator& endItr)
 {
-    int data;
-    std::cin >> data;
-
-    if (data == -1)
+    if (*itr == -1)
         return nullptr;
     
     Node* node = new Node;
-    node->key = data;
+    node->key = *itr;
     node->left = node->right = nullptr;
 
-    node->left = buildTree();
-    node->right = buildTree();
+    if (itr != endItr)
+        itr++;
+    node->left = buildTreePreorder(itr, endItr);
+    
+    if (itr != endItr)
+        itr++;
+    node->right = buildTreePreorder(itr, endItr);
 
     return node;
+}
+
+Node* buildTree(const std::vector<int>& treeMap)
+{
+    std::vector<int>::const_iterator start = treeMap.begin();
+    std::vector<int>::const_iterator end = treeMap.end();
+
+    return buildTreePreorder(start, end);
 }
 
 void printTree(Node* root)
@@ -127,8 +137,10 @@ int main(int argc, char** argv)
     }
 
     int level = std::stoi(argv[1]);
-    Node* root = buildTree();
-    //printTree(root);
+    std::vector<int> treeMap = {60, 50, 80, -1, -1, 10, -1, -1, 30, 40, -1, -1, -1};
+    //std::vector<int> treeMap = {2,7,-1,9,11,-1,-1,4,-1,-1,5,-1,1,-1,-1};
+    Node* root = buildTree(treeMap);
+    printTree(root);
     std::cout<<level<<superScript(level)+" level of tree: "<<printKthLevel(root, level)<<std::endl;
     
     return 0;
--- DSA_Exercises/BinaryTree/nodeSum.cpp
+++ DSA_Exercises/BinaryTree/nodeSum.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 struct Node
 {
@@ -6,22 +7,32 @@ struct Node
     Node* left, *right;
 };
 
-Node* buildTree()
+Node* buildTreePreorder(std::vector<int>::const_iterator& itr, const std::vector<int>::const_iterator& endItr)
 {
-    int data;
-    std::cin >> data;
-
-    if (data == -1)
+    if (*itr == -1)
         return nullptr;
     
     Node* node = new Node;
-    node->key = data;
+    node->key = *itr;
     node->left = node->right = nullptr;
 
-    node->left = buildTree();
-    node->right = buildTree();
+    if (itr != endItr)
+        itr++;
+    node->left = buildTreePreorder(itr, endItr);
+    
+    if (itr != endItr)
+        itr++;
+    node->right = buildTreePreorder(itr, endItr);
 
     return node;
+}
+
+Node* buildTree(const std::vector<int>& treeMap)
+{
+    std::vector<int>::const_iterator start = treeMap.begin();
+    std::vector<int>::const_iterator end = treeMap.end();
+
+    return buildTreePreorder(start, end);
 }
 
 int sumBT(Node* root)
@@ -37,7 +48,8 @@ int sumBT(Node* root)
 
 int main()
 {
-    Node* root = buildTree();
+    std::vector<int> treeMap = {2,7,-1,9,11,-1,-1,4,-1,-1,5,-1,1,-1,-1};
+    Node* root = buildTree(treeMap);
     std::cout<<"Binary tree node sum: "<<sumBT(root)<<std::endl;
     return 0;
 }
