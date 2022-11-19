b930f86fec3e77949c065ac725abd42371fcd8fd
Amol Dhamale <amoldhamale1105@gmail.com>
2022-11-19 23:42:21 +0530

    refactor reduced half array to an optimized and more elegant solution

--- DSA_Exercises/Heaps/reduceToHalf.cpp
+++ DSA_Exercises/Heaps/reduceToHalf.cpp
@@ -1,92 +1,46 @@
 #include <iostream>
 #include <vector>
-#include <set>
 #include <queue>
 #include <algorithm>
-
-class SubsetCompare
-{
-public:
-    bool operator()(const std::vector<int>& vec1, const std::vector<int>& vec2)
-    {
-        return vec1.size() > vec2.size();
-    }
-};
-
-static void uniqueSubsets(const std::vector<int>& input, std::vector<int>& output, int i, int j, std::vector<std::vector<int>>& subsets)
-{
-    //Base case
-    if (i >= input.size()){
-        output[j] = -1;
-        std::vector<int> subset;
-        for(auto itr = output.begin(); *itr != -1; ++itr)
-        {
-            subset.emplace_back(*itr);
-        }
-        if (!subset.empty()){
-            std::sort(subset.begin(), subset.end());
-            subsets.emplace_back(subset);
-        }
-        return;
-    }
-
-    //Rec case
-    output[j] = input[i];
-
-    uniqueSubsets(input, output, i+1, j+1, subsets);
-    uniqueSubsets(input, output, i+1, j, subsets);
-}
-
-static int getMatchCount(const std::vector<int>& sortedSubset, const std::vector<int>& sortedArr)
-{
-    int count{0}, subIndex{0};
-    int subsetSize = sortedSubset.size();
-
-    for(const int& element : sortedArr)
-    {
-        if (element == sortedSubset[subIndex])
-            count++;
-        else if (element > sortedSubset[subIndex]){
-            subIndex++;
-            if (subIndex >= subsetSize)
-                break;
-            if (element == sortedSubset[subIndex])
-                count++;
-        }
-    }
-
-    return count;
-}
+#include <ComplexObjectStream.hpp>
 
 static int minSetSize(const std::vector<int>& arr)
 {
-    std::set<int> uniqueElements;
-    std::vector<int> uniqueArr;
+    std::priority_queue<int> pq;
+    std::vector<int> elementFreq;
+
     for(const int& num : arr)
     {
-        uniqueElements.insert(num);
+        pq.push(num);
     }
-    for(const int& num : uniqueElements)
+    int count{0};
+    int currentElement = pq.top();
+    while (!pq.empty())
     {
-        uniqueArr.emplace_back(num);
+        while(!pq.empty() && currentElement == pq.top())
+        {
+            count++;
+            pq.pop();
+        }
+        elementFreq.emplace_back(count);
+        currentElement = pq.top();
+        count = 0;
     }
-
-    std::vector<int> output(arr.size()+1, 0);
-    std::vector<std::vector<int>> subsets;
-    uniqueSubsets(uniqueArr, output, 0, 0, subsets);
-
+    
+    // Sort in descending order
+    std::sort(elementFreq.begin(), elementFreq.end(), [](const int& a, const int& b)->bool{return a > b;});
+    int setCount{0}, sum{0};
     int halfSize = arr.size()/2;
-    std::vector<int> sortedArr = arr;
-    std::sort(sortedArr.begin(), sortedArr.end());
-    std::priority_queue<std::vector<int>, std::vector<std::vector<int>>, SubsetCompare> minSubsetHeap;
 
-    for(const std::vector<int>& subset : subsets)
+    for(const int& freqElement : elementFreq)
     {
-        if (getMatchCount(subset, sortedArr) >= halfSize)
-            minSubsetHeap.push(subset);
+        sum += freqElement;
+        setCount++;
+        if (sum >= halfSize)
+            break;
     }
 
-    return minSubsetHeap.top().size();
+    return setCount;
 }
 
 int main()
